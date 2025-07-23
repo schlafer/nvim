45086df95515fe6c2612368fef28fab5419f7b0f
Sazid Mahmud <sazidmahmud5@gmail.com>
2025-07-24 03:30:06 +0600

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode with jj" })
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+
